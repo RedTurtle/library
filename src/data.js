6c77d886721f5c63fc6aaa319adb7a43e911b1be
Piero Nicolli <piero.nicolli@yahoo.it>
2017-08-31 09:25:22 +0200

    Project created

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    name: 'collective.tiles.advancedstatic',
+    url: 'https://github.com/collective/collective.tiles.advancedstatic',
+    keywords: [
+      'plone',
+      'plone5',
+      'collective',
+      'static',
+      'tiles',
+      'advanced',
+    ],
+  },
+  {
+    name: 'collective.tiles.collection',
+    url: 'https://github.com/collective/collective.tiles.collection',
+    keywords: [
+      'plone',
+      'plone5',
+      'collective',
+      'collection',
+      'tiles',
+    ],
+  },
+];
