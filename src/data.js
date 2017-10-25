4976f386d99c5ba6c0f41d180fcfb40027310a0d
Piero Nicolli <piero.nicolli@redturtle.it>
2017-10-25 14:14:24 +0200

    Updated addons list

--- src/data.js
+++ src/data.js
@@ -8,6 +8,12 @@ export type DataItem = {
 
 const data: Array<DataItem> = [
   {
+    id: 'collective.lazysizes',
+    name: 'collective.lazysizes',
+    url: 'https://github.com/collective/collective.lazysizes',
+    keywords: ['plone', 'plone4', 'plone5', 'collective', 'lazysizes', 'lazyload'],
+  },
+  {
     id: 'collective.tiles.advancedstatic',
     name: 'collective.tiles.advancedstatic',
     url: 'https://github.com/collective/collective.tiles.advancedstatic',
@@ -24,6 +30,24 @@ const data: Array<DataItem> = [
     name: 'deployments.buildout.plone',
     url: 'https://github.com/RedTurtle/deployments.buildout.plone',
     keywords: ['plone', 'plone4', 'plone5', 'redturtle', 'rt', 'buildout'],
+  },
+  {
+    id: 'deployments.buildout.production',
+    name: 'deployments.buildout.production',
+    url: 'https://github.com/RedTurtle/deployments.buildout.production',
+    keywords: ['plone', 'plone4', 'plone5', 'redturtle', 'rt', 'buildout', 'production'],
+  },
+  {
+    id: 'plone.app.imagecropping',
+    name: 'plone.app.imagecropping',
+    url: 'https://github.com/collective/plone.app.imagecropping',
+    keywords: ['plone', 'plone5', 'collective', 'imagecropping', 'image'],
+  },
+  {
+    id: 'redturtle.tiles.management',
+    name: 'redturtle.tiles.management',
+    url: 'https://github.com/RedTurtle/redturtle.tiles.management',
+    keywords: ['plone', 'plone4', 'plone5', 'redturtle', 'rt', 'tiles', 'management', 'mosaic'],
   },
 ];
 
